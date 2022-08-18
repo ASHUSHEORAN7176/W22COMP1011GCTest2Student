c1417d20721d9ea1faf9134850ef1c5fb754c699
ASHUSHEORAN7176 <200485170@student,georgianc.on.ca>
2022-08-18 09:36:28 -0400

    Question 1 Complete

--- src/main/java/com/example/w22comp1011gctest2student/Customer.java
+++ src/main/java/com/example/w22comp1011gctest2student/Customer.java
@@ -1,5 +1,44 @@
 package com.example.w22comp1011gctest2student;
 
-public class Customer {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Customer {
+    private String firstName,lastName,phoneNumber;
+    private int id;
+    public ArrayList<Product> purchases;
+    List<Product> products;
+    public double totalPurchases;
+    public double totalPurchases(){
+        return purchases;
+    };
+
+    public Customer(String firstName, String lastName, String phoneNumber, int id, List<Product> products) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.id = id;
+        this.products = products;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
